990b8d74c0fb2673170fc7c43164120c4a3b11b7
reeloadead <reeloadead@gmail.com>
2022-08-29 19:06:33 +0300

    DateTimeMeasure, StopWatchMeasure

--- cs06/cs06/Program.cs
+++ cs06/cs06/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace cs06
+﻿using System.Diagnostics;
+
+namespace cs06
 {
     class Program
     {
         static void Main(string[] args)
         {
-            DateTimeMeasure(); // Datetime running calculations
+            Console.WriteLine("\nDatetime measure:");
+            DateTimeMeasure(); // Datetime running
+
+            Console.WriteLine("\nStopWatch measure:");
+            StopWatchMeasure(); // Stop Watch running
         }
 
         static void DateTimeMeasure()
@@ -21,6 +27,28 @@
             } 
 
             Console.WriteLine($"Время - {DateTime.Now - beforeStop}");
+        }
+
+        static void StopWatchMeasure()
+        {
+            var totalDuration = TimeSpan.Zero;
+            var sw = new Stopwatch();
+            uint num = 20;
+            for (uint i = 1; i <= num; i++)
+            {
+                uint count = 0;
+
+                sw.Restart();
+                uint a = Fibonacci.Fib1(i, ref count);
+                sw.Stop();
+
+                // uint a = Fib2(i, ref count);
+                // uint a = Fib3(i, ref count);
+                Console.WriteLine($"Номер числа: \t {i} \t Значение: \t {a} \t Количество вызовов: \t {count} \t Длительность: \t {sw.ElapsedTicks} ticks :");
+                totalDuration += sw.Elapsed;
+            } 
+
+            Console.WriteLine($"Время - {totalDuration}");
         }
     }
 }
